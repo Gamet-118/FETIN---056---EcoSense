f4173c8a4c67b4db5768944c2723a13891519f9f
graziellecyber <51826430+graziellecyber@users.noreply.github.com>
2026-08-13 18:26:21 -0300

    Remove App component implementation

--- Grazi/App.tsx
+++ Grazi/App.tsx
@@ -1,12 +1,1 @@
-import React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
-import DashboardScreen from "./src/screens/DashboardScreen";
 
-export default function App() {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
-      <DashboardScreen />
-    </SafeAreaView>
-  );
-}
